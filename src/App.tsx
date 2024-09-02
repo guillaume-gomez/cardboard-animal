b1e8745269bb538b587b58822b37f026260aa793
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-09-02 14:09:22 +0200

    update lib + app

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+      <div style={{width: "100%"}}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -25,12 +23,15 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <ThreeJsRenderer />
+        <div style={{border: "2px solid blue"}}>
+          <ThreeJsRenderer />
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      </div>
+
   )
 }
 
