a4f064c4c246c6c6f7de6068a9d91988b0cb5c44
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-09-08 22:04:55 +0200

    remove unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,16 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+            <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <div style={{border: "2px solid blue"}}>
-          <ThreeJsRenderer />
+        <h1>Vite + React</h1>
+        <div className="card">
+          <div style={{border: "2px solid blue"}}>
+            <ThreeJsRenderer />
+          </div>
         </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </div>
-
-  )
+  );
 }
 
 export default App
